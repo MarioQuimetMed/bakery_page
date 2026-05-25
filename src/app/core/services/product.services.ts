893a9b783bc21ad6d7ff1c9de21fc035c27606a2
Paulo <paulopachury7@gmail.com>
2026-05-25 00:39:44 -0400

    Creacion componente producto y servicio

---
+++ src/app/core/services/product.services.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Producto {
+  id?: number;
+  nombre: string;
+  descripcion: string;
+  url_image: string;
+  created_at?: Date;
+  updated_at?: Date;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private apiUrl = 'http://localhost:3000/api/productos'; 
+
+  constructor(private http: HttpClient) {}
+  
+  //LISTAR PRODUCTOS 
+
+  //CREAR PRODUCTO
+
+  // --- EDITAR PRODUCTO ---
+  updateProduct(id: number, producto: Producto): Observable<Producto> {
+    return this.http.put<Producto>(`${this.apiUrl}/${id}`, producto);
+  }
+
+  // --- ELIMINAR PRODUCTO ---
+  deleteProduct(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
